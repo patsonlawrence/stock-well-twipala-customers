8312eb4e22b5298c228601e17a777789e0c66164
Patson T Lawrence <patsont90@gmail.com>
2025-11-24 19:47:26 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,33 @@ export const viewport = {
   themeColor: "#0d9488",
 };
 
-export default function RootLayout({
+/*export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">      
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}*/
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/faviconv1.ico" />
+        <meta name="theme-color" content="#0d9488" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
